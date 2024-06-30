a3f6b757aa9443cb9f896ba92e0fe05a8680daaf
vadolaxt <22130304@st.hcmuaf.edu.vn>
2024-06-30 23:01:15 +0700

    problem 13- not done

--- src/solutions/easy/RomanToInteger.java
+++ src/solutions/easy/RomanToInteger.java
@@ -1,14 +1,46 @@
 package solutions.easy;
 
 public class RomanToInteger {
-    public int romanToInt(String s) {
-        int rs = 0;
-
-
-        return rs;
+    public static int romanToInt(String s) {
+        int prev = 0, num = 0, ans = 0;
+        char[] arr = s.toCharArray();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            switch (arr[i]) {
+                case 'I':
+                    num = 1;
+                    break;
+                case 'V':
+                    num = 5;
+                    break;
+                case 'X':
+                    num = 10;
+                    break;
+                case 'L':
+                    num = 50;
+                    break;
+                case 'C':
+                    num = 100;
+                    break;
+                case 'D':
+                    num = 500;
+                    break;
+                case 'M':
+                    num = 1000;
+                    break;
+            }
+            // Nếu số hiện tại nhỏ hơn số liền kề bên phải
+            if (num < prev) {
+                ans -= num;
+                // Nếu số hiện tại lớn hơn hoặc bằng số liền kề bên phải
+            } else {
+                ans += num;
+            }
+            prev = num;
+        }
+        return ans;
     }
-
     public static void main(String[] args) {
-
+        System.out.println(romanToInt("XVIII"));
+        System.out.println(romanToInt("XIV"));
     }
 }
